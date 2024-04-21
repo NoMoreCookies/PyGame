16a67f4c67da9e354948766ff5245bacb7653207
Kacper <ekacperop.com@gmail.com>
2024-04-21 16:38:27 +0200

    Added makeoin Xses and Os

--- TicTacToe/TicTacToe.py
+++ TicTacToe/TicTacToe.py
@@ -14,6 +14,12 @@ pygame.display.set_caption("TicTacToe")
 #define variables
 line_width = 2
 markers = []
+clicked = Falsepos = []
+player = 1
+
+#define colours
+green = (0,255,0)
+red = (255,0,0)
 
 def draw_grid():
 
@@ -29,14 +35,40 @@ for x in range(3):
     row = [0] * 3
     markers.append(row)
 
-while run:
+def draw_markers():
+    x_pos = 0
+    for x in markers:
+        y_pos = 0
+        for y in x:
+            if y == 1:
+                pygame.draw.line(screen,green,( x_pos*100 + 15,y_pos * 100 + 15 ),(x_pos*100 + 85,y_pos*100+85),line_width)
+                pygame.draw.line(screen,green,( x_pos*100 + 15,y_pos * 100 + 85 ),(x_pos*100 + 85,y_pos*100+15),line_width)
+            if y == -1:
+                pygame.draw.circle(screen, red, (x_pos * 100 + 50 , y_pos * 100 + 50),38,line_width)
+            y_pos += 1
+        x_pos +=1
 
+
+while run:
     draw_grid()
+    draw_markers()
 
     #add event handlers
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
+        #jeżeli kli
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            clicked = True
+        if event.type == pygame.MOUSEBUTTONUP and clicked == True:
+            clicked == False
+            pos = pygame.mouse.get_pos()
+            cell_x = pos[0]
+            cell_y = pos[1]
+            if markers[cell_x // 100][cell_y // 100] == 0:
+                markers[cell_x // 100][cell_y // 100] = player
+                player *= -1
+                
 
     pygame.display.update()
 
